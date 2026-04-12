a9a0d3c6b436c4ec8098e5574466c035007a56b7
fansypants <balthasarspann@gmail.com>
2026-04-12 10:39:10 +0200

    Add files via upload

---
+++ sw(1).js
@@ -0,0 +1,10 @@
+// Network-first: always try to get fresh content, fall back to cache
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
